55a6c627554995d9898907e34d25565facb1b4cd
三ッ國拓真 <lupin.create@hotmail.co.jp>
2014-07-14 17:01:25 +0900

    修正版

--- KMR.py
+++ KMR.py
@@ -22,7 +22,7 @@ for i in range(1, N+1): #行動１→行動０への変更
     ratio = np.array([1-m_i, m_i])
     exp = np.dot(profit_A, ratio)
     if exp[0] > exp[1]: #行動０が最適反応のとき
-        p[i][i-1] = m_i*((1.0-epsilon) + epsilon*0.5)    # p[i][i-1] x_0が１減少するマスの確率
+        p[i][i-1] = m_i*((1.0-epsilon) + epsilon*0.5)     # p[i][i-1] x_0が１減少するマスの確率
     else:
         p[i][i-1] = m_i*(epsilon*0.5)
 for i in range(N): #行動０→行動１への変更
@@ -43,6 +43,6 @@ p[N][N] = 1 - p[N][N-1]
     
     
     
-X = mc_sample_path(p, x_0, t)   #マルコフ連鎖
+X = mc_sample_path(p, x_0, times)   #マルコフ連鎖
 plt.plot(X)
 plt.show()
